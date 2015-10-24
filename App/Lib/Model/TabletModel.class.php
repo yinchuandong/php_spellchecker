b4790bc73dc684c5da99061d94a94afbc9ea83e6
yinchuandong <yincd520@sina.com>
2015-10-24 23:25:58 +0800

    change the model, add some validate

--- App/Lib/Model/TabletModel.class.php
+++ App/Lib/Model/TabletModel.class.php
@@ -1,14 +1,20 @@
 <?php
 class TabletModel extends Model{
 	
+	protected $_validate = array(
+    	array('mac','require','not null'), //默认情况下用正则进行验证
+    	array('mac','','already exists',0,'unique',1)
+	);
 	
+	protected $_auto = array (
+			array('time', 'getTime', Model::MODEL_INSERT, 'function'),
+	);
+
 	public function __construct(){
 		parent::__construct();
 	}
 	
-	// protected $_auto = array (
-	// 		array('time', 'getTime', Model::MODEL_INSERT, 'function'),
-	// );
+	
 	
 } 
 
